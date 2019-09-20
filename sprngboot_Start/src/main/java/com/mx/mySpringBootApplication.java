f47ef3f0ff0ce4da7806a87d2d8fc08d437192e2
mx <986082688@qq.com>
2019-09-20 16:50:51 +0800

    1

---
+++ sprngboot_Start/src/main/java/com/mx/mySpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.mx;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(mySpringBootApplication.class);
+    }
+}
